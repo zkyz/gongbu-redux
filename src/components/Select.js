8349ea0104481f3981d4a45b69f18c55e8bab217
tester <tester@cheoeum.in>
2017-04-12 14:41:26 +0900

    Start with Idiommatic Redux by Dan Abramov

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,50 +1,66 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {SelectField} from 'redux-form-material-ui'
-import {Field} from 'redux-form'
-import {pickBy} from 'lodash'
-import {MenuItem} from 'material-ui'
+import React from "react";
+import PropTypes from "prop-types";
+import {SelectField} from "redux-form-material-ui";
+import {Field} from "redux-form";
+import {pickBy} from "lodash";
+import {MenuItem} from "material-ui";
 
-const Select = (attr) => {
-	const {id, name, style, label, options} = attr
-	const otherAttr = pickBy(attr, (x, y) => y !== 'label' && y !== 'options')
+class Select extends React.Component {
 
-	let key = 0
+    constructor(props) {
+        super(props)
 
-	return (
-		<Field { ...otherAttr }
-					 id={ id }
-					 name={ name }
-					 floatingLabelText={ label || id || name }
-					 component={ SelectField }
-					 style={{...style, verticalAlign: 'top'}}
-		>
-			{
-				options.map(
-					option => <MenuItem key={ key++ }
-															value={ option.value }
-															primaryText={ option.text }
+        this.props = props
+        this.options = props.options
+        this.otherAttr = pickBy(props, (x, y) => y !== 'label' && y !== 'options')
+    }
 
-					/>
-				)
-			}
-		</Field>
-	)
+    render() {
+        const {id, name, label, style} = this.props
+        let key = 0
+
+        new Promise(fx => setTimeout(fx, 500)).then(() => {
+            this.options = [
+                {value: 123, text: 'test'},
+                {value: 456, text: 'check'}
+            ];
+        })
+
+        return (
+            <Field { ...this.otherAttr }
+                   id={ id }
+                   name={ name }
+                   floatingLabelText={ label || id || name }
+                   component={ SelectField }
+                   style={ {...style, verticalAlign: 'top'} }
+            >
+                {
+                    this.options.map(
+                        option => <MenuItem key={ key++ }
+                                            value={ option.value }
+                                            primaryText={ option.text }
+
+                        />
+                    )
+                }
+            </Field>
+        )
+    }
 }
 
 Select.propTypes = {
-	name:      PropTypes.string.isRequired,
-	id:        PropTypes.string,
-	label:     PropTypes.string,
-	style:     PropTypes.object,
-	options:   PropTypes.arrayOf(
-		PropTypes.shape({
-			value: PropTypes.any.isRequired,
-			text:  PropTypes.string.isRequired
-		})
-	),
-	validate:  PropTypes.func,
-	autoFocus: PropTypes.bool
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    style: PropTypes.object,
+    options: PropTypes.arrayOf(
+        PropTypes.shape({
+            value: PropTypes.any.isRequired,
+            text: PropTypes.string.isRequired
+        })
+    ),
+    validate: PropTypes.func,
+    autoFocus: PropTypes.bool
 }
 
 export default Select
